7fc4d7e7990f4bfb9be61a1c9b6673b25ea137ad
ReeceBank <reesevdbank@gmail.com>
2020-10-07 18:31:54 +0200

    merged alwyn and chleo + minigame
    everything seemes to work. minigame just needs to be intergrated/loaded via buttons

---
+++ C19game/Assets/Scripts/clickLogic.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickLogic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Click()
+    {
+        //access the boolean values of each card presented
+        GameObject cardAreaOne = GameObject.Find("symptomOne");
+        placeCards cardPlayedOne = cardAreaOne.GetComponent<placeCards>();
+        bool valueOne = cardPlayedOne.coronaSymptomatic;
+
+        GameObject cardAreaTwo = GameObject.Find("symptomTwo");
+        placeCards cardPlayedTwo = cardAreaTwo.GetComponent<placeCards>();
+        bool valueTwo = cardPlayedTwo.coronaSymptomatic;
+
+        GameObject cardAreaThree = GameObject.Find("SymptomThree");
+        placeCards cardPlayedThree = cardAreaThree.GetComponent<placeCards>();
+        bool valueThree = cardPlayedThree.coronaSymptomatic;
+
+        //logic operation to determine correctness of answer
+        bool result = valueOne || valueTwo || valueThree;
+
+        if (result)
+        {
+            Debug.Log("CORRECT!");
+
+        }
+        else {
+            Debug.Log("WRONG!!!!!!!!");
+        }
+    }
+}
---
+++ C19game/Assets/placeCards.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class placeCards : MonoBehaviour
+{
+    //list of symptom game objects
+
+    //corona symptoms
+    public GameObject fever;
+    public GameObject fatigue;
+    public GameObject cough;
+
+    //fine symptom
+    public GameObject fine;
+
+    //other symptoms
+    public GameObject hair;
+    public GameObject feet;
+    public GameObject thirst;
+
+    public GameObject cardDisplay;
+
+    //variable to log if corona symptomatic
+    public bool coronaSymptomatic;
+
+    List<GameObject> deck = new List<GameObject>();
+
+    void Awake()
+    {
+        //add cards to deck
+        deck.Add(fever);
+        deck.Add(fatigue);
+        deck.Add(cough);
+        deck.Add(fine);
+        deck.Add(hair);
+        deck.Add(feet);
+        deck.Add(thirst);
+
+        //generate a random card in a card space
+        for (var i = 0; i < 1; i++)
+        {
+            //get a random card
+            int index = Random.Range(0, deck.Count);
+
+            //if the index is 0,1 or 2 then its a corona virus symptom
+            if (index == 0)
+            {
+                coronaSymptomatic = true;
+            }
+
+            else if (index==1)
+            {
+                coronaSymptomatic = true;
+            }
+
+            else if (index==2)
+            {
+                coronaSymptomatic = true;
+            }
+
+            //if the index is any other number then no coronavirus symptomatic
+            else {
+                coronaSymptomatic = false;
+            }
+
+            //place card
+            GameObject symptomCard = Instantiate(deck[index], new Vector3(0, 0, 0), Quaternion.identity);
+            symptomCard.transform.SetParent(cardDisplay.transform, false);
+
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    } 
+}

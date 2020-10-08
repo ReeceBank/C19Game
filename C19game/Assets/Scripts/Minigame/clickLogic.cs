1c49bd6ed29f24b5ea3e08e1f39c9bbe302329e4
ReeceBank <reesevdbank@gmail.com>
2020-10-09 01:46:01 +0200

    intergrated minigame with main
    wip undate, not finished but saving.

---
+++ C19game/Assets/Scripts/Minigame/clickLogic.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickLogic : MonoBehaviour
+{
+    bool result;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool GetResult()
+    {
+        //access the boolean values of each card presented
+        GameObject cardAreaOne = GameObject.Find("symptomOne");
+        placeCards cardPlayedOne = cardAreaOne.GetComponent<placeCards>();
+        bool valueOne = cardPlayedOne.coronaSymptomatic;
+
+        GameObject cardAreaTwo = GameObject.Find("symptomTwo");
+        placeCards cardPlayedTwo = cardAreaTwo.GetComponent<placeCards>();
+        bool valueTwo = cardPlayedTwo.coronaSymptomatic;
+
+        GameObject cardAreaThree = GameObject.Find("SymptomThree");
+        placeCards cardPlayedThree = cardAreaThree.GetComponent<placeCards>();
+        bool valueThree = cardPlayedThree.coronaSymptomatic;
+
+        //logic operation to determine correctness of answer
+        result = (valueOne || valueTwo || valueThree);
+        return result;
+
+    }
+
+    //determines click logic for each button to half script size
+    public void YesClick()
+    {
+        if (GetResult()) //calls above function to see if button clicked is Yes
+        {
+            Debug.Log("CORRECT!");
+
+        }
+        else
+        {
+            Debug.Log("WRONG!!!!!!!!");
+        }
+
+    }
+
+    public void NoClick() //inverse logic for when the button clicked is No
+    {
+        if (!GetResult())
+        {
+            Debug.Log("CORRECT!");
+
+        }
+        else
+        {
+            Debug.Log("WRONG!!!!!!!!");
+        }
+
+    }
+}
--- C19game/Assets/Scripts/PHSceneLoaderMaintoGame.cs
+++ C19game/Assets/Scripts/PHSceneLoaderMaintoGame.cs
@@ -5,8 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class PHSceneLoaderMaintoGame : MonoBehaviour
 {
-    public void LoadProtoGame()
+    public void LoadProtoGame() //depreciated, use LoadOneForward
+    {
+        LoadOneForward();
+    }
+    public void LoadOneForward() //for going forward a single scene
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void LoadOneBack() // for going back a single scene
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 }

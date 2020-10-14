7eb0034b48ed12a3770f59812068490c112f0f3f
HHLie <53212860+HHLie@users.noreply.github.com>
2020-10-14 19:22:49 +0200

    Added ROI bar
    Similar to the health bar

---
+++ C19game/Assets/Scripts/ShoppingLevel/ROI_Bar.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ROI_Bar : MonoBehaviour
+{
+    public Slider ROI;
+    public int currentROI = 0;
+    public int maxROI = 30;
+
+    [SerializeField]
+    public Text level;
+
+    void Awake()
+    {
+        ROI = GetComponent<Slider>();
+
+    }
+    void Update()
+    {
+
+        ROI.value = currentROI;
+        ROI.maxValue = maxROI;
+        
+        if (currentROI >= 20)
+        {
+            level.text = "HIGH";
+        }
+        if (currentROI <= 19 && currentROI >= 10)
+        {
+            level.text = "MED";
+        }
+        if (currentROI <= 9)
+        {
+            level.text = "LOW";
+        }
+
+    }
+    public void changeROI(int dHP)
+    {
+        if (currentROI + dHP > 30)
+        {
+            currentROI = 30;
+            // do hp dmg
+
+        }
+        else if (currentROI + dHP < 0)
+        {
+            
+            currentROI = 0;
+
+            
+        }
+        else
+        {
+            currentROI += dHP;
+        }
+
+
+    }
+    public void setMaxROI(int maxHP)
+    {
+        maxROI = maxHP;
+    }
+    public void setCurrentROI(int currentHP)
+    {
+        currentROI = currentHP;
+    }
+}

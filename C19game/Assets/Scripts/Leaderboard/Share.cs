c94643e554ba659872b368404411b1a89d75cd11
HHLie <53212860+HHLie@users.noreply.github.com>
2020-10-15 19:22:13 +0200

    updated share button

---
+++ C19game/Assets/Scripts/Leaderboard/Share.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Share : MonoBehaviour
+{
+
+    
+
+    [SerializeField]
+    public Text share_text;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void click()
+    {
+        StartCoroutine(Error_message());
+    }
+
+    IEnumerator Error_message()
+    {
+        share_text.text = "GooglePlay login Failed";
+        yield return new WaitForSeconds(3);
+        share_text.text = "";
+    }
+}

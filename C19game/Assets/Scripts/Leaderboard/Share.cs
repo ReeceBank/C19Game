db62ee3489b01fb0a68e7bee1481c33851592f6f
ReeceBank <reesevdbank@gmail.com>
2020-10-16 14:04:03 +0200

    added share functionality that takes a screenshot and edits the text of a share message, like on twitter

--- C19game/Assets/Scripts/Leaderboard/Share.cs
+++ C19game/Assets/Scripts/Leaderboard/Share.cs
@@ -2,27 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
 
 public class Share : MonoBehaviour
 {
-    [SerializeField]
-    public Text share_text;
+    private string shareMessage;
+	private leaderboard leaderboard;
 
-    /*
-     * on click we get error message
-     */
-    public void click()
+    public void Start()
     {
-        StartCoroutine(Error_message());
+		leaderboard = GameObject.Find("Leaderboard").GetComponent<leaderboard>();
     }
+    public void shareClick()
+    {
+		shareMessage = "Look who scored a whopping " + leaderboard.get_highscore()[1] + " points in Cards Against Coronavirus, can anyone do better?";
+		
+		Debug.Log(shareMessage);
 
-    /*
-     *display error message 
-     */
-    IEnumerator Error_message()
-    {
-        share_text.text = "GooglePlay login Failed";
-        yield return new WaitForSeconds(3);
-        share_text.text = "";
+		StartCoroutine(TakeScreenshotAndShare());
     }
+	private IEnumerator TakeScreenshotAndShare()
+	{
+		yield return new WaitForEndOfFrame();
+
+		Texture2D ss = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+		ss.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+		ss.Apply();
+
+		string filePath = Path.Combine(Application.temporaryCachePath, "shared img.png");
+		File.WriteAllBytes(filePath, ss.EncodeToPNG());
+
+		// To avoid memory leaks
+		Destroy(ss);
+
+		new NativeShare().AddFile(filePath)
+			.SetSubject("Cards Against Coronavirus").SetText(shareMessage)
+			.SetCallback((result, shareTarget) => Debug.Log("Share result: " + result + ", selected app: " + shareTarget))
+			.Share();
+	}
 }
